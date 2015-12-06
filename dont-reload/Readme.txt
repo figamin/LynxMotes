4acac51d92be441dcf326a116c4aa98cd7510688
Sérgio Augusto Vianna <sergio.a.vianna@gmail.com>
2015-12-06 13:19:04 -0200

    Initial commit.

---
+++ dont-reload/Readme.txt
@@ -0,0 +1,3 @@
+This addons will turn < into orange text, adding the class orangeText.
+
+Remember to add this class to your css, since it isn't on the placeholder front-end.
---
+++ index.js
@@ -0,0 +1,38 @@
+//Adds the css class orangeText if user posts <text
+
+var common = require('../../engine/postingOps').common;
+
+exports.engineVersion = '1.3.2';
+
+var orangeTextFunction = function(match) {
+
+  var content = match.substring(8);
+  return '<span class="orangeText">&lt' + content + '</span>';
+
+};
+
+exports.init = function() {
+
+  var originalMarkdown = common.markdownText;
+
+  common.markdownText = function(message, board, replaceCode, callback) {
+
+    message = message.replace(/</g, '[orange]');
+
+    originalMarkdown(message, board, replaceCode, callback);
+
+  };
+
+  var originalProcessLine = common.processLine;
+
+  common.processLine = function(split, replaceCode) {
+
+    split = originalProcessLine(split, replaceCode);
+
+    split = split.replace(/^\[orange\][^\&].*/g, orangeTextFunction);
+
+    return split.replace(/\[orange\]/g, '&lt');
+
+  };
+
+};
